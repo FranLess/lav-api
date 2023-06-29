97568f2c6f414849e6448d3a32311ab5e470775d
FranLess <franciscoLessMore@gmail.com>
2023-06-28 21:21:55 -0600

    Last commit

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware([
-    // 'auth:sanctum'
+    'auth:sanctum'
 ])->prefix('v1')->group(function () {
     RouteHelper::includeRouteFiles(__DIR__ . '/api/v1');
     // require __DIR__ . '/api/v1/comments.php';
